34bb07dab34535d8874cd2186ffc112cdba55564
Tim Escher <tim@eschers.com>
2013-02-07 11:12:34 -0600

    Mail fixing still

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ EscherHomewatch::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # ActionMailer settings
-  config.action_mailer.default_url_options = { :host => "https://shielded-castle-3741.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "shielded-castle-3741.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
