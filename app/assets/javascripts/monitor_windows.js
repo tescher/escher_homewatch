c1d4c8778206b5fb237e2953e007e79a760b7684
Tim Escher <tim@eschers.com>
2013-02-28 13:37:53 -0600

    Sort table entries

--- app/assets/javascripts/monitor_windows.js
+++ app/assets/javascripts/monitor_windows.js
@@ -32,6 +32,8 @@ function MonitorWindow(config, windowDiv) {
                 var formatted = '<span id="legend">' + label + " (";
                 var last = series['data'].length - 1;
                 if (last >= 0) {
+                    console.log(now_utc.valueOf());
+                    console.log(series['data'][last][0]);
                     if ((now_utc.valueOf() - series['data'][last][0]) > 60*60*1000) {  //If data more than an hour old, signify
                         old = true;
                     }
