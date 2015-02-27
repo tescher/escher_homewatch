ad804ddac6b2a55cc3510907393681e35d49ad0d
Tim Escher <tim@eschers.com>
2015-02-27 07:23:20 -0800

    Mail settings for WebFaction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ EscherHomewatch::Application.configure do
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
     # config.action_mailer.default_options = {from: 'no-reply@escherhomewatch.com'}
-    ActionMailer::Base.default :from => 'no-reply@escherhomewatch.com'
+    # ActionMailer::Base.default :from => 'no-reply@escherhomewatch.com'
 
   else
     config.action_mailer.delivery_method = :smtp
