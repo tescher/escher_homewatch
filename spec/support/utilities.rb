e77cced705ec1a6f956d09e13b78bd5e0b2989ce
Tim Escher <tim@eschers.com>
2016-12-26 04:18:07 -0900

    Some testing fixes

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,4 +1,5 @@
 include ApplicationHelper
+include SessionsHelper
 
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
@@ -29,6 +30,8 @@ end
 
 
 def valid_signin(user)
+  pp user
+  #sign_in user
   visit signin_path
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
