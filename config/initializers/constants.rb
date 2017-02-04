140f6952d24acbfd0f934fb94c5c5934812bc20e
Tim Escher <tim@eschers.com>
2017-02-03 18:40:32 -0900

    Allow multiple emails on alerts

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,5 @@
 # E-mail formatter
-VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+VALID_EMAIL_REGEX = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+)*$/i
 REQUEST_KEY_MAGIC = 271
 DEFAULT_TIME_ZONE = "Central Time (US & Canada)"
 
